d3692d3675fe89f74a68784ec801b80aef8f19a4
Kristin Houchin <kmhouchin@gmail.com>
2015-07-16 10:51:25 -0500

    Click link to show info works. Need to fix later is the a on info

--- public/global.js
+++ public/global.js
@@ -28,49 +28,65 @@
 
       for (var i = 0; i < req.response.length; i++) {
   
-        var li = document.createElement("li");
+        var ul = document.createElement("ul");
+        
   
         list = JSON.parse(req.response[i]);
         // li.innerHTML =list.id + " " + list.name
-        assignment_list.appendChild(li);
+        assignment_list.appendChild(ul);
         var a = link_assignment(list.id, list.name)
-        li.appendChild(a);
-      };
-    };
+        a.setAttribute("data-info", "info" + list.id)
+        ul.appendChild(a);
+        var li = document.createElement("li")
+        li.className ="info";
+        li.setAttribute("id", "info" + list.id)
+        li.innerText = list.name; 
+        a.appendChild(li);
+                
 
-    /////////////////////////////////////////////
-    //link for assignment name
-    /////////////////////////////////////////////
+        
+        //   if (info.className == "info"){
+          //   info.className("info show");
+          // }
+          // else{
+            //     info.className("show");
+            //   }
+          };
+      
+        };
 
-    var link_assignment = function(id, name){
-      var a = document.createElement("a");
-      a.setAttribute("href", ("/api/assignments/" + id));
-      a.addEventListener("click", information)
-      a.appendChild(document.createTextNode("Assignment Name: " + name));
-      return a;
-    };
+        /////////////////////////////////////////////
+        //link for assignment name
+        /////////////////////////////////////////////
 
-    /////////////////////////////////////////////
-    //display one assignment information
-    //////////////////////////////////////////
-    var information = function(){
-      var req = new XMLHttpRequest();
-      req.open("get", this.href);
-      req.addEventListener("load", function(){
-        document.getElementById("name").innerHTML = req.response.name;
-        document.getElementById("description").innerHTML = req.response.description;
-        document.getElementById("github_link").innerHTML = req.response.description;
-    
-      })
-  
-      event.preventDefault();
-      req.responseType = "json";
-      req.send();
-  
-    }
+        var link_assignment = function(id, name, event){
+          var a = document.createElement("a");
+          a.setAttribute("href", ("/api/assignments/" + id));
+          a.addEventListener("click", information)
+          a.appendChild(document.createTextNode("Assignment Name: " + name));
+          return a;
+        };    
+        var information = function(){
+          // var req = new XMLHttpRequest();
+          // req.open("get", this.href);
+          var info_id = this.getAttribute("data-info")
+          list = document.getElementById(info_id);
+          list.classList.toggle("show");
+      
+          // req.addEventListener("load", function() {
+            //   document.getElementById("assignments").innerText = "";
+            //   document.getElementById("title").innerHTML = req.response.name;
+            //   document.getElementById("description").innerHTML = req.response.description;
+            //
+            // })
+            event.preventDefault();
+            // req.responseType = "json";
+            // req.send();
+          }
+      
 
 
-    // var strJSON = req.response;
-    // var objJSON = eval("(function(){return " + strJSON + ";})()");
-    //
-    // assignment_list.innerText = objJSON.github_link
+          // var strJSON = req.response;
+          // var objJSON = eval("(function(){return " + strJSON + ";})()");
+          //
+          // assignment_list.innerText = objJSON.github_link
